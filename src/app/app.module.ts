a06c05ded74f351b3e1363277eede7b39c0e8739
Erick Gonzalez <erickgozan@gmail.com>
2020-06-28 22:50:38 -0500

    Correcion de la direccion de la carpeta pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { Error404Component } from './error404/error404.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { FooterComponent } from './pages/footer/footer.component';
+import { Error404Component } from './pages/error404/error404.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
