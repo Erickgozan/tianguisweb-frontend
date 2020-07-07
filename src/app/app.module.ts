265386335fb43409576b697b686ef8c612dd20e0
Erick Gonzalez <erickgozan@gmail.com>
2020-07-07 15:43:50 -0500

    Se agregaron los componentes menu y producto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { HeaderComponent } from './pages/header/header.component';
 import { FooterComponent } from './pages/footer/footer.component';
 import { Error404Component } from './pages/error404/error404.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProductoComponent } from './producto/producto.component';
+import { MenuComponent } from './pages/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    Error404Component
+    Error404Component,
+    ProductoComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
