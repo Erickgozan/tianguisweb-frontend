3b875610bd7c1c0604c2878c8b405c8ea5a830fa
Erick Gonzalez <erickgozan@gmail.com>
2020-07-16 01:40:24 -0500

    Se agrego la ruta del componente listaProductos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HeaderComponent } from './pages/header/header.component';
 import { FooterComponent } from './pages/footer/footer.component';
 import { Error404Component } from './pages/error404/error404.component';
 import { ProductoComponent } from './producto/producto.component';
-
-
+import { ListaProductosComponent } from './pages/lista-productos/lista-productos.component';
 
 const routes: Routes = [
   //Ruta principal
- // {path:'', redirectTo:'header', pathMatch:'full'},
+  //{path:'', redirectTo:'lista-productos', pathMatch:'full'},
+  {path:'',  component:ListaProductosComponent},
   //ruta encabezado
   {path:'productos', component:ProductoComponent},
   //Ruta pie de pagina
