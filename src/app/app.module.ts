15e3600c73004ee0a60fef318f6a7cd3b115ee3f
Erick Gonzalez <erickgozan@gmail.com>
2020-07-16 01:41:43 -0500

    Se agregaron lista productos y el slider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './pages/header/header.component';
 import { FooterComponent } from './pages/footer/footer.component';
 import { Error404Component } from './pages/error404/error404.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ProductoComponent } from './producto/producto.component';
 import { MenuComponent } from './pages/menu/menu.component';
+import { ListaProductosComponent } from './pages/lista-productos/lista-productos.component';
+import { SliderComponent } from './pages/slider/slider.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
+    SliderComponent,
     FooterComponent,
     Error404Component,
     ProductoComponent,
-    MenuComponent
+    MenuComponent,
+    ListaProductosComponent
   ],
   imports: [
     BrowserModule,
