c4458b5522e5681eadcd3f8989fede0e4a43da12
erick <erickgozan@gmail.com>
2021-02-18 23:24:51 -0600

    Se agrego el metodo para guardar la direccion

--- src/app/service/direccion.service.ts
+++ src/app/service/direccion.service.ts
@@ -1,18 +1,34 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
-import { map } from "rxjs/operators";
+import { Observable, throwError } from "rxjs";
+import { catchError } from "rxjs/operators";
 import { Direccion } from "../entity/direccion";
 
 @Injectable({
   providedIn: "root",
 })
 export class DireccionService {
-  private urlEndPoint: string = "https://api-sepomex.hckdrk.mx/query/info_cp";
+  
+  private urlEndPointApiDireccion: string = "https://api-sepomex.hckdrk.mx/query/info_cp";
+  private urlEndPoint:string = "http://localhost:8080/api/direcciones"
 
   constructor(private http: HttpClient) {}
 
-  public obtenerDireciones(cp: number): Observable<any>{
-    return this.http.get<any>(`${this.urlEndPoint}/${cp}`);
+  public getAddress(cp: number): Observable<any>{
+    return this.http.get<any>(`${this.urlEndPointApiDireccion}/${cp}`);
+  }
+
+  public saveAddress(direccion:Direccion):Observable<Direccion>{
+      return this.http.post<Direccion>(`${this.urlEndPoint}/save`,direccion).pipe(
+          catchError((err)=>{
+              if(err.status == 400 && err.error_400){
+                return throwError(err);
+              }
+              if(err.status == 500 && err.error_500){
+                return throwError(err);
+              }
+              return throwError(err);
+          })
+        )
   }
 }
