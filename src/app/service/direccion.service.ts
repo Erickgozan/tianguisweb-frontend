bf85b99585dab843a029b51118f9cbccadd9eaeb
erick <erickgozan@gmail.com>
2021-02-22 20:43:27 -0600

    Se agrego el metodo getAddresById y se modifico el metodo saveAdress

--- src/app/service/direccion.service.ts
+++ src/app/service/direccion.service.ts
@@ -8,27 +8,42 @@ import { Direccion } from "../entity/direccion";
   providedIn: "root",
 })
 export class DireccionService {
-  
-  private urlEndPointApiDireccion: string = "https://api-sepomex.hckdrk.mx/query/info_cp";
-  private urlEndPoint:string = "http://localhost:8080/api/direcciones"
+  private urlEndPointApiDireccion: string =
+    "https://api-sepomex.hckdrk.mx/query/info_cp";
+  private urlEndPoint: string = "http://localhost:8080/api/direcciones";
 
   constructor(private http: HttpClient) {}
 
-  public getAddress(cp: number): Observable<any>{
+  //Obtener la dirección del api externa
+  public getAddress(cp: number): Observable<any> {
     return this.http.get<any>(`${this.urlEndPointApiDireccion}/${cp}`);
   }
 
-  public saveAddress(direccion:Direccion):Observable<Direccion>{
-      return this.http.post<Direccion>(`${this.urlEndPoint}/save`,direccion).pipe(
-          catchError((err)=>{
-              if(err.status == 400 && err.error_400){
-                return throwError(err);
-              }
-              if(err.status == 500 && err.error_500){
-                return throwError(err);
-              }
-              return throwError(err);
-          })
-        )
+  //Obtener la direccion de la urlEndPoint
+  public getAddressById(id:number):Observable<Direccion>{
+     return this.http.get<Direccion>(`${this.urlEndPoint}/${id}`).pipe(
+       catchError((err)=>{
+          if(err.status==404 && err.error.error_404){
+            return throwError(err);
+          }
+          return throwError(err);
+       })
+     )
+  }
+
+  public saveAddress(direccion: Direccion): Observable<Direccion> {
+    return this.http
+      .post<Direccion>(`${this.urlEndPoint}/save`, direccion)
+      .pipe(
+        catchError((err) => {
+          if (err.status == 400 && err.error.error_400) {
+            return throwError(err);
+          }
+          if (err.status == 500 && err.error.error_500) {
+            return throwError(err);
+          }
+          return throwError(err);
+        })
+      );
   }
 }
