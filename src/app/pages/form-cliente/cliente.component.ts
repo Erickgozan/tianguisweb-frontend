ad11753c0dda8c18bc719aecb26e088d79db3c1a
erick <erickgozan@gmail.com>
2021-02-22 20:36:57 -0600

    Se agregaron nuevos atributos

--- src/app/pages/form-cliente/cliente.component.ts
+++ src/app/pages/form-cliente/cliente.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { ActivatedRoute } from "@angular/router";
 import { Cliente } from "src/app/entity/cliente";
 import { Direccion } from "src/app/entity/direccion";
 import { ClienteService } from "src/app/service/cliente.service";
@@ -13,13 +14,35 @@ import Swal from "sweetalert2";
 })
 export class ClienteComponent implements OnInit {
   public cliente: Cliente;
+  public direccion:Direccion;
+  public idDireccion:number;
 
-  constructor(private clienteService: ClienteService) {
+  constructor(private clienteService: ClienteService, 
+    private direccionService:DireccionService,
+    private activateRoute:ActivatedRoute) {
     this.cliente = new Cliente();
+    this.direccion = new Direccion();
+    console.log(this.idDireccion);
+    
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    //this.getDireccion();
+  }
 
+
+  /*public getDireccion():void{
+      this.activateRoute.params.subscribe((param)=>{
+        let id = param.id;
+        if(id != null){
+          this.direccionService.getAddressById(id).subscribe(direccion=>{
+            this.direccion = direccion;
+            console.log(direccion);
+            
+          })
+        }
+      })
+  }*/
   //Crear clientes
   public crearClientes(): void {
     this.clienteService.saveCustomer(this.cliente).subscribe(
