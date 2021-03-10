7e0abad320af69a76051e58f98ed0edcc11a4c73
erick <erickgozan@gmail.com>
2021-03-10 17:39:28 -0600

    Se modifico el componente

--- src/app/pages/lista-productos/lista-productos.component.ts
+++ src/app/pages/lista-productos/lista-productos.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { ProductoService } from "src/app/service/producto.service";
-import { ActivatedRoute } from "@angular/router";
 import { Producto } from "src/app/entity/producto";
-import Swal from "sweetalert2";
 
 @Component({
   selector: "app-lista-productos",
@@ -13,9 +11,7 @@ export class ListaProductosComponent implements OnInit {
   public productos: Array<Producto>;
 
   constructor(
-    public productoService: ProductoService,
-    private activateRoute: ActivatedRoute
-  ) {}
+    public productoService: ProductoService  ) {}
 
   ngOnInit(): void {
     //Suscripcion al listados de productos
