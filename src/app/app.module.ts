63a421f17a6268af5b54cbe874601d49cf55046b
Erick <erickgozan@gmail.com>
2021-03-17 00:25:14 -0600

    Se establecio un orden

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { FooterComponent } from './pages/footer/footer.component';
 import { Error404Component } from './pages/error404/error404.component';
@@ -11,7 +12,6 @@ import { MenuComponent } from './pages/menu/menu.component';
 import { ListaProductosComponent } from './pages/lista-productos/lista-productos.component';
 import { SliderComponent } from './pages/slider/slider.component';
 import { ProductoComponent } from './pages/form-producto/producto.component';
-import { HttpClientModule } from '@angular/common/http';
 import { CategoriaComponent } from './pages/categoria/categoria.component';
 import { DetalleProductoComponent } from './pages/detalle-producto/detalle-producto.component';
 import { CarritoProductoComponent } from './pages/carrito-producto/carrito-producto.component';
