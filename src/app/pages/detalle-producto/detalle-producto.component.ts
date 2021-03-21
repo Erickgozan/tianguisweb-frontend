8382ee27f64f3c2cb83941c306c799708cfbe959
Erick <erickgozan@gmail.com>
2021-03-20 22:05:47 -0600

    Se agrego el metodo para generar la compra

--- src/app/pages/detalle-producto/detalle-producto.component.ts
+++ src/app/pages/detalle-producto/detalle-producto.component.ts
@@ -4,31 +4,38 @@ import { ItemProducto } from "src/app/entity/itemProducto";
 import { Pedido } from "src/app/entity/pedido";
 import { Producto } from "src/app/entity/producto";
 import { CarritoService } from "src/app/service/carrito.service";
+import { CompraService } from "src/app/service/compra.service";
 import { ProductoService } from "src/app/service/producto.service";
+import Swal from "sweetalert2";
 @Component({
   selector: "app-detalle-producto",
   templateUrl: "./detalle-producto.component.html",
+  styleUrls: ["./detalle-producto.component.css"],
+
 })
 export class DetalleProductoComponent implements OnInit {
-  
+
   public producto: Producto;
   public pedido: Pedido;
   public imagen: string;
   public total: number;
-  public cantidad:number=1;
+  public cantidad: number = 1;
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private productoService: ProductoService,
-    public carritoService: CarritoService
+    public carritoService: CarritoService,
+    public compraService: CompraService
   ) {
     this.producto = new Producto();
     this.pedido = this.carritoService.getPedido();
+
   }
 
   ngOnInit(): void {
+
     this.cargarProducto();
-    }
+  }
 
   //Muestra el producto que en base a su id
   public cargarProducto(): void {
@@ -37,7 +44,10 @@ export class DetalleProductoComponent implements OnInit {
       if (id != null) {
         this.productoService
           .findProductById(id)
-          .subscribe((producto) => (this.producto = producto));
+          .subscribe((producto) => {
+            this.producto = producto
+            this.total = this.producto.precio;
+          });
       }
     });
   }
@@ -50,26 +60,54 @@ export class DetalleProductoComponent implements OnInit {
   }
 
   //Genera el pedido
-  public generarPedido(producto:Producto): void {   
-    this.carritoService.addProdcutoDetalle(producto,this.cantidad);
+  public generarPedido(producto: Producto): void {
+
+    this.pedido.itemProductos = new Array();
+
+    const productosItem = this.pedido.itemProductos.find(items => {
+      return items.producto.id === producto.id;
+    })
+
+    if (productosItem != undefined) {
+      productosItem.cantidad = this.cantidad;
+    } else {
+      const item: ItemProducto = {
+        cantidad: this.cantidad,
+        producto: producto
+      };
+      this.pedido.precioTotal = this.total;
+      this.pedido.itemProductos.push(item);
+    }
+
+    Swal.fire({
+      title: 'Confirmar la compra?',
+      text: `${producto.nombre}`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si lo quiero!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.compraService.generarCompra(this.pedido).subscribe();
+        Swal.fire(
+          `Ya es tuyo ${this.pedido.cliente.nombre}!`,
+          'Hemos confirmado tu compra.',
+          'success'
+        );
+        this.carritoService.eliminarItem(producto.id);
+      }
+    });
+    console.log(this.pedido);
+
   }
+
+
+
 
   //Selecciona y muestra la imagen del producto.
   public seleccionarImg(img: string): void {
     this.imagen = img;
   }
 
-  /**
-   * 
-let itemProducto = this.pedido.productos.find((item) => {
-      return item.producto.id === producto.id;
-    });
-
-    if (itemProducto == undefined) {
-      this.carritoService.addProductoPedido(producto);
-    } else {
-      this.carritoService.addProductoPedidoChange(producto, event);
-    }
-    console.log(this.itemProducto);
-   */
 }
