f85523a28830abaca07d125a80d6b858921cd9ae
Erick <erickgozan@gmail.com>
2021-04-05 12:46:07 -0500

    Se implemento el cierre de sesion

--- src/app/pages/menu/menu.component.ts
+++ src/app/pages/menu/menu.component.ts
@@ -3,8 +3,10 @@ import { Router } from "@angular/router";
 import { Categoria } from "src/app/entity/categoria";
 import { Pedido } from "src/app/entity/pedido";
 import { Producto } from "src/app/entity/producto";
+import { AuthService } from "src/app/service/Auth.service";
 import { CarritoService } from "src/app/service/carrito.service";
 import { ProductoService } from "src/app/service/producto.service";
+import Swal from "sweetalert2";
 
 @Component({
   selector: "app-menu",
@@ -17,11 +19,12 @@ export class MenuComponent implements OnInit {
   public ruta: string;
   public producto:Producto;
   public pedido:Pedido;
-
+  public isAdmin:boolean=false;
   constructor(
     public router: Router,
     private productoService: ProductoService,
-    public carritoService:CarritoService
+    public carritoService:CarritoService,
+    public authService:AuthService
   ) {
     this.producto = new Producto();
     this.pedido = this.carritoService.getPedido();
@@ -32,6 +35,7 @@ export class MenuComponent implements OnInit {
     
   }
 
+
   //Listar las categorias
   public cargarCategoria(): void {
     this.productoService.categoriaList().subscribe((categorias) => {
@@ -39,5 +43,25 @@ export class MenuComponent implements OnInit {
     });
   }
 
+
+  public logout():void{
+
+
+    Swal.fire({
+      title: 'Quieres cerrar tu sesión?',
+      text:`Hasta la proxima ${this.authService.usuario.username}`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, cerrar!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire("Logout",`Hola ${this.authService.usuario.username} has cerrado sesión con éxito`,"success")
+        this.router.navigate(["cliente/login"]);
+        this.authService.logout();
+      }
+    });
+  }
   
 }
