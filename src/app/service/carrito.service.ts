71fcabd48a4ac09a00d99ec7e4299676c6e0c1b0
Erick <erickgozan@gmail.com>
2021-04-05 12:49:50 -0500

    Se agrego el cliente y el metodo addProductoDetalle

--- src/app/service/carrito.service.ts
+++ src/app/service/carrito.service.ts
@@ -5,6 +5,7 @@ import { EstadoPedido } from "../entity/estadoPedido.enum";
 import { ItemProducto } from "../entity/itemProducto";
 import { Pedido } from "../entity/pedido";
 import { Producto } from "../entity/producto";
+import { AuthService } from "./Auth.service";
 import { ProductoService } from "./producto.service";
 
 @Injectable({
@@ -14,7 +15,8 @@ export class CarritoService {
   private pedido: Pedido;
   private producto: Producto;
 
-  constructor(private productoService: ProductoService) {
+  constructor(private productoService: ProductoService,
+    private authService:AuthService) {
     this.pedido = new Pedido();
     this.producto = new Producto();
     this.initCarrito();
@@ -22,7 +24,7 @@ export class CarritoService {
 
   //Inicializar los valores del pedido
   private initCarrito(): void {
-    this.pedido.id="";
+    this.pedido.id = "";
     this.pedido.cliente = null;
     this.pedido.itemProductos = new Array();
     this.pedido.precioTotal = null;
@@ -32,8 +34,12 @@ export class CarritoService {
     this.cargarStorage();
   }
 
+  //Crear un metodo getter para obtener el precio original desde la base de datos
+  
+
   //Agregar producto en el pedido y en el localStorage
   public addProductoPedido(producto: Producto) {
+    
     this.producto = producto;
 
     let item = this.pedido.itemProductos.find((productos) => {
@@ -42,7 +48,8 @@ export class CarritoService {
 
     if (item !== undefined) {
       item.cantidad++;
-      item.producto.precio = item.cantidad * this.producto.precio;
+      item.producto.precio = (this.producto.precio * item.cantidad);
+      
     } else {
       const addProductoCarrito: ItemProducto = {
         cantidad: 1,
@@ -51,21 +58,22 @@ export class CarritoService {
       this.pedido.itemProductos.push(addProductoCarrito);
     }
 
-    localStorage.setItem("pedido", JSON.stringify(this.pedido));
-
-    this.contarElemtosCarrito();
+    this.pedido.cliente = this.authService.usuario;
+    //Sumar el gran total
     this.sumarPrecioTotal();
+
+    localStorage.setItem("pedido", JSON.stringify(this.pedido));
   }
 
   //Incrementar la cantidad del producto o eliminarlo desde el carrito de compras
   public addProductoPedidoChange(producto: Producto, event: any) {
     let cantidad = event.target.value as number;
-  
+   
     //Eliminar si la cantidad de productos es igual a 0
     if (cantidad == 0) {
       this.eliminarItem(producto.id);
     }
-    
+
     //Incrementa o disminuye la cantidad de preductos del itemProducto
     //Y modifica el precio del producto
     this.pedido.itemProductos = this.pedido.itemProductos.map((item) => {
@@ -74,28 +82,41 @@ export class CarritoService {
         //Multiplicar el precio original por la cantidad de productos
         this.productoService.findProductById(producto.id).subscribe((jsonProducto) => {
           this.producto = jsonProducto;
-          item.producto.precio = jsonProducto.precio * cantidad;
+          item.producto.precio = this.producto.precio * item.cantidad;
+          
+          //Sumar total
+          this.sumarPrecioTotal()
+
+          //Agregar el pedido al local storage
+          localStorage.setItem("pedido", JSON.stringify(this.pedido));
         });
-        
+
       }
       return item;
     });
-    //Calcular el precio total      
-    this.sumarPrecioTotal();
-    //Agregar el pedido al local storage
-    localStorage.setItem("pedido", JSON.stringify(this.pedido));
+    this.pedido.cliente = this.authService.usuario;
   }
 
   //Agregar el pedido desde el detalle del producto
-  public addProdcutoDetalle(producto:Producto, cantidad:number):void{
+  public addProductoDetalle(producto: Producto, cantidad: number, total:number): void {
 
-    const itemProducto = new ItemProducto();
-    itemProducto.cantidad = cantidad;
-    itemProducto.producto = producto;
+    this.pedido.itemProductos = new Array();
+    const productosItem = this.pedido.itemProductos.find(items => {
+      return items.producto.id === producto.id;
+    });
 
-    this.pedido.itemProductos.push(itemProducto);
-
-    localStorage.setItem("pedido",JSON.stringify(this.pedido));
+    if (productosItem != undefined) {
+      productosItem.cantidad = cantidad;
+    } else {
+      const item: ItemProducto = {
+        cantidad: cantidad,
+        producto: producto
+      };
+      this.pedido.precioTotal = total;
+      this.pedido.itemProductos.push(item);
+    }
+    this.pedido.cliente = this.authService.usuario;
+    localStorage.setItem("pedido", JSON.stringify(this.pedido));
   }
 
   //Eliminar itemProducto
@@ -127,8 +148,10 @@ export class CarritoService {
     this.pedido.itemProductos.forEach((itemPreudctos) => {
       total += Number(itemPreudctos.producto.precio);
     });
+
     this.pedido.precioTotal = total;
   }
+
 
   //Mantiene el producto en el localStorage
   private cargarStorage(): Pedido {
