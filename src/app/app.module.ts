65878b87cf2d39fa214902e0eecd98b1e8e47461
Erick <erickgozan@gmail.com>
2021-04-10 16:38:43 -0500

    Se agregaron todos los modulos sociales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
+//Modulos
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//Componentes
 import { AppComponent } from './app.component';
 import { FooterComponent } from './pages/footer/footer.component';
 import { Error404Component } from './pages/error404/error404.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MenuComponent } from './pages/menu/menu.component';
 import { ListaProductosComponent } from './pages/lista-productos/lista-productos.component';
 import { SliderComponent } from './pages/slider/slider.component';
@@ -18,9 +19,17 @@ import { CarritoProductoComponent } from './pages/carrito-producto/carrito-produ
 import { TarjetaProductoComponent } from './pages/tarjeta-producto/tarjeta-producto.component';
 import { ClienteComponent } from './pages/form-cliente/cliente.component';
 import { LoginComponent } from './pages/login/login.component';
+import { FormDireccionComponent } from './pages/form-direccion/form-direccion.component';
+//Interceptores
 import { TokenInterceptor } from './entity/interceptors/token.interceptor';
 import { AuthInterceptor } from './entity/interceptors/auth.interceptor';
-import { FormDireccionComponent } from './pages/form-direccion/form-direccion.component';
+
+//Social login
+import { SocialLoginModule, SocialAuthServiceConfig } from 'angularx-social-login';
+import {
+  GoogleLoginProvider,
+  FacebookLoginProvider
+} from 'angularx-social-login';
 
 
 @NgModule({
@@ -46,9 +55,31 @@ import { FormDireccionComponent } from './pages/form-direccion/form-direccion.co
     BrowserAnimationsModule,
     HttpClientModule,
     FormsModule,
+    SocialLoginModule
   ],
-  providers: [{provide:HTTP_INTERCEPTORS,useClass:TokenInterceptor,multi:true},
-              {provide:HTTP_INTERCEPTORS,useClass:AuthInterceptor,multi:true},],
+  providers: [{ provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true },
+  { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+  //Social
+  {
+    provide: 'SocialAuthServiceConfig',
+    useValue: {
+      autoLogin: false,
+      providers: [
+        {
+          id: GoogleLoginProvider.PROVIDER_ID,
+          provider: new GoogleLoginProvider(
+            '366236921494-l9lat44ktmk287utr3gj8hs8nbdail7m.apps.googleusercontent.com'
+          )
+        },
+        {
+          id: FacebookLoginProvider.PROVIDER_ID,
+          provider: new FacebookLoginProvider('3743722089058250')
+        }
+      ]
+    } as SocialAuthServiceConfig,
+  }
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
