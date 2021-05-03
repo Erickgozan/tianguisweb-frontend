e984cf19c65397b58beab9bb0fa8ab8ff2f7c6e2
Erick Gonzalez <erickgozan@gmail.com>
2021-05-03 15:25:55 -0500

    Se modifico el slider para visualizar las imgs

--- src/app/pages/slider/slider.component.ts
+++ src/app/pages/slider/slider.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import { Slider } from "src/app/entity/slider";
+import { SliderService } from "src/app/service/slider.service";
 
 @Component({
   selector: "app-slider",
@@ -6,7 +8,23 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./slider.component.css"],
 })
 export class SliderComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit(): void {}
+  public sliders:Array<Slider>;
+  public url:string="http://localhost:8080/api/view/img/";
+
+  constructor(private sliderService:SliderService) {
+    this.sliders = new Array();
+  }
+
+  ngOnInit(): void {
+    this.getAllSliders();  
+  } 
+
+  //Guarda el array de sliders
+  public getAllSliders(){
+    this.sliderService.listSlider().subscribe(jSliders=>{
+      this.sliders = jSliders;
+    });
+  }
+
 }
