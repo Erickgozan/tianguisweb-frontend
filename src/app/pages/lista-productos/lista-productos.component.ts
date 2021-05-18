630df7b5f62d9e4e87a542dade72268628698e6d
Erick Gonzalez <erickgozan@gmail.com>
2021-05-18 15:08:50 -0500

    Se agrego la logica de busqueda

--- src/app/pages/lista-productos/lista-productos.component.ts
+++ src/app/pages/lista-productos/lista-productos.component.ts
@@ -1,22 +1,55 @@
 import { Component, OnInit } from "@angular/core";
 import { ProductoService } from "src/app/service/producto.service";
 import { Producto } from "src/app/entity/producto";
+import { ActivatedRoute } from "@angular/router";
+
 
 @Component({
   selector: "app-lista-productos",
   templateUrl: "./lista-productos.component.html",
-  styleUrls: ["./lista-productos.component.css"], 
+  styleUrls: ["./lista-productos.component.css"],
 })
 export class ListaProductosComponent implements OnInit {
-  public productos: Array<Producto>;
 
-  constructor(
-    public productoService: ProductoService  ) {}
+  public productos: Array<Producto>;
+  public productoPaginador: any[];
+  public filtro_valor: string = "";
+
+
+  constructor(public productoService: ProductoService,
+    private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
-    //Suscripcion al listados de productos
-    this.productoService
-      .puductList()
-      .subscribe((productos) => (this.productos = productos));
+
+    this.activatedRoute.paramMap.subscribe(params => {
+      this.filtro_valor = params.get("nombre");
+      if (this.filtro_valor == null && this.productos == undefined) {
+        this.listadoPaginado();
+      } else {
+        this.listadoPorNombre();
+      }
+    });
   }
+
+  //Listados de productos paginados
+  listadoPaginado(): void {
+    this.activatedRoute.paramMap.subscribe(params => {
+      let page: number = +params.get("page");
+      if (!page) {
+        page = 0;
+      }
+      this.productoService.puductList(page).subscribe(json => {
+        (this.productos = json.content as Producto[]),
+          this.productoPaginador = json;
+      });
+    });
+  }
+
+  //Listado paginado de busqueda por nombre
+  listadoPorNombre(): void {
+    this.productoService.findProductoByNombre(this.filtro_valor).subscribe(jproductos => {
+        this.productos = jproductos;
+    });
+  }
+
 }
