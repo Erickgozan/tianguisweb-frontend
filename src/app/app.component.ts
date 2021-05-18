f6e22ba7bdb2b7f436794c22aa7168c6b929eefc
Erick Gonzalez <erickgozan@gmail.com>
2021-05-18 15:02:53 -0500

    Se creo el metodo handleSearch($event)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Producto } from './entity/producto';
+import { ProductoService } from './service/producto.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'tianguisweb';
+export class AppComponent{
+  
+  public filtro_valor: string = "";
+
+  constructor(private router: Router) { }
+
+  //Metodo que recupera el texto del input
+  handleSearch(value: string): void {
+    this.filtro_valor = value;
+    if (this.filtro_valor) {
+      this.router.navigate(['productos/buscar/', this.filtro_valor]);
+    }
+  }
+
 }
