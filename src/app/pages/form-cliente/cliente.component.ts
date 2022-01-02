7612dbe5c08a721de0bb35baab83283d117ec74b
Erick Gonzalez <erickgozan@gmail.com>
2022-01-02 13:24:14 -0600

    Se realizaron actualizaciones en el cliente

--- src/app/pages/form-cliente/cliente.component.ts
+++ src/app/pages/form-cliente/cliente.component.ts
@@ -1,7 +1,9 @@
 import { Component, Input, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validator, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
 import { Cliente } from "src/app/entity/cliente";
 import { Role } from "src/app/entity/role";
+import { Usuario } from "src/app/entity/usuario";
 import { AuthService } from "src/app/service/Auth.service";
 import { ClienteService } from "src/app/service/cliente.service";
 import { RoleService } from "src/app/service/role.service";
@@ -18,6 +20,9 @@ export class ClienteComponent implements OnInit {
   public cliente: Cliente;
   public role_user: Role;
   public errores: Array<string>;
+  public rutaActual: string;
+  public rutaUpdate: string;
+  public id: string;
 
   public form: FormGroup;
 
@@ -27,16 +32,41 @@ export class ClienteComponent implements OnInit {
     private roleService: RoleService,
     private formBuilder: FormBuilder,
     private validadores: ValidadoresService,
-    private authService:AuthService
+    private authService: AuthService,
+    public router: Router
   ) {
     this.cliente = new Cliente();
     this.role_user = new Role();
     this.cliente.roles = new Array();
+    this.rutaActual = router.url;
+    this.id = this.rutaActual.split("/")[3];
+    this.rutaUpdate = "/cliente/form/" + this.id;
   }
 
   ngOnInit(): void {
     this.crearFormulario();
     this.listarRoles();
+    if (this.id != undefined) {
+      this.ObtenerClientePorId();
+    }
+  }
+
+  //Obtener el cliente
+  public ObtenerClientePorId(): void {
+    this.clienteService.findCustumerById(this.id).subscribe(jCliente => {
+      this.cliente = jCliente;
+      this.form.reset({
+        nombre: this.cliente.nombre,
+        apellido: this.cliente.apellido,
+        email: this.cliente.email,
+        telefono: this.cliente.telefono
+      });
+
+    }, err => {
+      if (err.status == 404) {
+        this.router.navigate(["cliente/login"]);
+      }
+    });
   }
 
   //Construir el formulario
@@ -45,7 +75,6 @@ export class ClienteComponent implements OnInit {
       nombre: ['', [Validators.required, Validators.minLength(5)]],
       apellido: ['', [Validators.required, Validators.minLength(5)]],
       telefono: ['', [Validators.required]],
-      username: ['', [Validators.required]],
       email: ['', [Validators.required, Validators.pattern("^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$")]],
       password: ['', [Validators.required, Validators.minLength(8)]],
       password2: ['', [Validators.required]],
@@ -66,9 +95,6 @@ export class ClienteComponent implements OnInit {
   public get celularInvalido(): boolean | undefined {
     return this.form.get('telefono')?.invalid && this.form.get('telefono')?.touched
   }
-  public get usuarioInvalido(): boolean | undefined {
-    return this.form.get('username')?.invalid && this.form.get('username')?.touched
-  }
   public get emailInvalido(): boolean | undefined {
     return this.form.get('email')?.invalid && this.form.get('email')?.touched
   }
@@ -88,7 +114,7 @@ export class ClienteComponent implements OnInit {
   //Obtener los roles y hablitar al cliente
   public listarRoles(): void {
     this.roleService.findAllRoles().subscribe(roles => {
-      this.role_user = roles[1];   
+      this.role_user = roles[1];
       this.cliente.roles.push(this.role_user);
       this.cliente.habilitado = true;
     });
@@ -104,8 +130,8 @@ export class ClienteComponent implements OnInit {
       );
 
     } else {
-       this.construirCliente();
-       this.clienteService.saveCustomer(this.cliente).subscribe(
+      this.construirCliente();
+      this.clienteService.saveCustomer(this.cliente).subscribe(
         () => {
         }, (err) => {
           this.errores = err.error.error_400 as Array<string>;
@@ -119,26 +145,55 @@ export class ClienteComponent implements OnInit {
         }
       );
 
-      Swal.fire("Registro", `Bienvenido ${this.cliente.username} te has registrado con éxito.`, 'success')
+      Swal.fire("Registro", `Bienvenido ${this.cliente.nombre} te has registrado con éxito.`, 'success')
         .then(result => {
           if (result.isConfirmed) {
-            this.authService.login(this.cliente).subscribe(jUsuario=>{
+            this.authService.login(this.cliente).subscribe(jUsuario => {
               this.authService.guardarUsuario(jUsuario.access_token);
-               this.authService.guardarToken(jUsuario.access_token);
-               history.back();
+              this.authService.guardarToken(jUsuario.access_token);
+              history.back();
             });
           }
         });
     }
   }
 
+  //Actualizar contraseña
+  public actualizarContra() {
+    if (this.form.invalid) {
+      return Object.values(this.form.controls).forEach(control => {
+        control.markAsTouched();
+      });
+    }
+    let password:string = this.form.get("password").value;
+      
+    this.clienteService.updatePassword(password, this.id).subscribe(
+      () => {
+        Swal.fire("Actualización de contraseña", "Listo tu contraseña fue restablecida.", "success")
+        .then(result=>{
+            if(result.isConfirmed){
+              this.router.navigate(["/"]);
+            }
+        })
+      }, err => {
+        if (err.status == 404 && err.error.error_404) {
+          Swal.fire("Error", `${err.error.error_404}`, "error");
+        }
+        if (err.status == 500 && err.error.error_500) {
+          Swal.fire("Error", `${err.error.error_500}`, "error");
+        }
+      })
+
+
+  }
+
   //Establecer al cliente
-  private construirCliente():void{   
+  private construirCliente(): void {
     this.cliente.nombre = this.form.get("nombre").value;
     this.cliente.apellido = this.form.get("apellido").value;
     this.cliente.email = this.form.get("email").value;
-    this.cliente.username = this.form.get("username").value;
     this.cliente.telefono = this.form.get("telefono").value;
-    this.cliente.password = this.form.get("password").value;   
+    this.cliente.password = this.form.get("password").value;
   }
+
 }
