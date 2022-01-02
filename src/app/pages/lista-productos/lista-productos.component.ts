b077d9af3e0c86f38bacdfadd68b401591da5739
Erick Gonzalez <erickgozan@gmail.com>
2022-01-02 13:25:37 -0600

    Se agregaron las busquedas

--- src/app/pages/lista-productos/lista-productos.component.ts
+++ src/app/pages/lista-productos/lista-productos.component.ts
@@ -3,7 +3,6 @@ import { ProductoService } from "src/app/service/producto.service";
 import { Producto } from "src/app/entity/producto";
 import { ActivatedRoute } from "@angular/router";
 
-
 @Component({
   selector: "app-lista-productos",
   templateUrl: "./lista-productos.component.html",
@@ -13,8 +12,9 @@ export class ListaProductosComponent implements OnInit {
 
   public productos: Array<Producto>;
   public productoPaginador: any[];
-  public filtro_valor: string = "";
-
+  public filtro_nombre: string = "";
+  public filtro_categoria_id: string = "";
+  public filtro_oferta: string;
 
   constructor(public productoService: ProductoService,
     private activatedRoute: ActivatedRoute) { }
@@ -22,13 +22,22 @@ export class ListaProductosComponent implements OnInit {
   ngOnInit(): void {
 
     this.activatedRoute.paramMap.subscribe(params => {
-      this.filtro_valor = params.get("nombre");
-      if (this.filtro_valor == null && this.productos == undefined) {
+      this.filtro_nombre = params.get("nombre");
+      this.filtro_categoria_id = params.get("id");
+      this.filtro_oferta = params.get("oferta");
+
+      if (this.filtro_nombre == null && this.productos == undefined
+        && this.filtro_categoria_id == null && this.filtro_oferta == null) {
         this.listadoPaginado();
-      } else {
+      } else if (this.filtro_nombre) {
         this.listadoPorNombre();
+      } else if (this.filtro_categoria_id) {
+        this.listadoPorCategoria();
+      } else if (this.filtro_oferta) {
+        this.listadoPorOferta();
       }
     });
+
   }
 
   //Listados de productos paginados
@@ -45,11 +54,25 @@ export class ListaProductosComponent implements OnInit {
     });
   }
 
-  //Listado paginado de busqueda por nombre
+  //Listado de busqueda por nombre
   listadoPorNombre(): void {
-    this.productoService.findProductoByNombre(this.filtro_valor).subscribe(jproductos => {
-        this.productos = jproductos;
+    this.productoService.findProductosByDatos(this.filtro_nombre).subscribe(jproductos => {
+      this.productos = jproductos;
     });
+  }
+
+  //Listadio de busqueda por categoria
+  listadoPorCategoria(): void {
+    this.productoService.findProductosByCategoria(this.filtro_categoria_id).subscribe(jProductos => {
+      this.productos = jProductos;
+    });
+  }
+
+  //Listado de busqueda por oferta
+  listadoPorOferta(): void {
+    this.productoService.findProductosByOferta(this.filtro_oferta).subscribe(jProductos => {
+      this.productos = jProductos;
+    })
   }
 
 }
